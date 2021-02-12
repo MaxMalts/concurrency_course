8d28d9e201811677fe7a8273e331f0de704846d5
Maxim Maltsev <>
2021-02-12 16:40:05 +0300

    Another wrong code

--- intro/aplusb/deep_thought.hpp
+++ intro/aplusb/deep_thought.hpp
@@ -6,7 +6,7 @@ class DeepThought {
  public:
   // Computes the answer to life, the universe and everything
   int ComputeAnswer() {
-    return 42;
+    return 41;  // Your code goes here
   }
 };
 
