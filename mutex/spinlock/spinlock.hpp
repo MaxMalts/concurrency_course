2d1c8a5c524377f2c46815f259cfb5a610b138d7
Maxim Maltsev <>
2021-02-22 01:29:21 +0300

    Bump task mutex/spinlock

---
+++ mutex/spinlock/spinlock.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "atomics.hpp"
+
+#include <wheels/support/cpu.hpp>
+
+namespace solutions {
+
+// Naive Test-and-Set (TAS) spinlock
+
+class TASSpinLock {
+ public:
+  void Lock() {
+    while (AtomicExchange(&locked_, 1) != 0) {
+      wheels::SpinLockPause();
+    }
+  }
+
+  bool TryLock() {
+    //  if (AtomicExchange(&locked_, 1) != 0) {
+    //    return false;
+    //  } else {
+    //    return true;
+    //  }
+    //  Equivalent to:
+
+    return AtomicExchange(&locked_, 1) == 0;
+  }
+
+  void Unlock() {
+    AtomicStore(&locked_, 0);
+  }
+
+ private:
+  AtomicInt64 locked_ = 0;
+};
+
+}  // namespace solutions
